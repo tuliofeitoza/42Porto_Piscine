5f2a2bdd58e2446d4ed2b0075020c5f6aefd6bf8
tuliofeitoza <tuliosfeitoza@gmail.com>
2024-01-31 03:51:25 +0000

    C05 Exercicio 00 realizado

---
+++ Projects_C/C05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsilva-f <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/31 03:41:50 by tsilva-f          #+#    #+#             */
+/*   Updated: 2024/01/31 03:44:29 by tsilva-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_iterative_factorial(int nb)
+{
+	int	fact;
+
+	fact = 1;
+	if (nb < 0)
+		return (0);
+	if (nb <= 1)
+		return (fact);
+	while (nb > 0)
+	{
+		fact *= nb;
+		nb--;
+	}
+	return (fact);
+}
