bdaf38b8df7f331e4ae1b9cc3cabe205398e1b6c
tuliofeitoza <tuliosfeitoza@gmail.com>
2024-01-31 03:35:53 +0000

    C04 Realizado suficiente

---
+++ Projects_C/C04/ex02/ft_putnbr.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsilva-f <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/31 03:06:18 by tsilva-f          #+#    #+#             */
+/*   Updated: 2024/01/31 03:18:04 by tsilva-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write( 1, &c, 1);
+}
+
+void	ft_punbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		putchar('-');
+		putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+		ft_putnbr(nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putchar(nb / 10);
+		ft_putchar(nb % 10);
+	}
+	else
+	{
+		putchar(nb + 48);
+	}
+}
